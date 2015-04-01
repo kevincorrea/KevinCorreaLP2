bcdd86bcd7a3fdb42ad4be9c69d62f84e1bae70d
Kevin <kevin131kevin@gmail.com>
2015-04-01 18:28:13 -0300

    Exercícios

---
+++ Exercícios/Aula_1703(3)/2Exercicio30032015/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace _2Exercicio30032015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList digitos = new ArrayList();
+            int soma = 0;
+            int media = 0;
+            int n = 1;
+         
+            Console.WriteLine("Escreva seus números, digite 0 para parar e obter a média dos números: ");
+
+            while (n != 0)
+            {
+
+                n = int.Parse(Console.ReadLine());
+                
+                digitos.Add(n);
+                digitos.Remove(0);
+
+                soma = n + soma;
+
+            }
+
+           
+               media = soma / digitos.Count;
+               Console.WriteLine("A média é igual a {0}", media);
+            
+
+
+        }
+    }
+}
---
+++ Exercícios/Aula_1703/Exercicio30032015/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio30032015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int n = 1;
+            int x = 0;
+            int soma = 0;
+            int media = 0;
+
+            Console.WriteLine("Escreva seus números, digite 0 para parar e obter a média dos números: ");
+
+            while (n != 0)
+            {
+
+                n = int.Parse(Console.ReadLine());
+                soma = n + soma;
+                x++;
+
+            }
+
+            if (n == 0)
+            {
+                media = soma / x;
+                Console.WriteLine("A média é igual a {0}", media);
+            }
+           
+        }
+    }
+}
