16b131e6f1a478136f2adade8a662a22c8ed5b3b
Kevin <kevin131kevin@gmail.com>
2015-06-29 17:11:48 -0300

    Aula 2306 - Class e DateTime

---
+++ Exercícios/Aula_2306 (Class e DataTime)/Aula_2306 (Class e DataTime)/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_2306__Class_e_DataTime_
+{
+    class Funcionario
+    {
+    }
+}
