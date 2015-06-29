a024456413b80b6a335203307a5c8bc3862cd605
Kevin <kevin131kevin@gmail.com>
2015-06-29 17:20:46 -0300

    Aula2306 - Class and DateTime

---
+++ Exercícios/Aula_2306 (Class e DataTime)/Aula_2306 (Class e DataTime)/Funcionario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_2306__Class_e_DataTime_
+{
+    class Funcionario
+    {
+        public string nome;
+        public string cpf;
+        public DateTime admi;
+        public DateTime nasc;
+        public double salario;
+        double aum;
+
+        public double CalculaAumento()
+        {
+            if (admi.Year <= 2014)
+            {
+                aum = 423.45;
+            }
+
+            if (admi.Year >= 2010 && admi.Year <= 2013)
+            {
+                aum = (salario / 100) * 9.84;
+            }
+
+            if (admi.Year >= 2000 && admi.Year <= 2009)
+            {
+                aum = (salario / 100) * 16.4;
+            }
+
+            if (admi.Year <= 1999)
+            {
+                aum = (salario / 100) * 22.6;
+            }
+
+            return aum;
+        }
+
+    }
+}
