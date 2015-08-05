2aefb6681ed07cef341049abbc639a7fc88ff28f
Kevin <kevin131kevin@gmail.com>
2015-08-04 22:04:16 -0300

    Exercicio 10

---
+++ Exercícios/Exercicio10/Exercicio10/Carro.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio10
+{
+    class Carro
+    {
+        public string _placa;
+        public string _modelo;
+        public string _marca;
+        public double _ano;
+         public double _kilometragem;
+         public double _captanque;
+         public double _potencia;
+         
+      
+
+        
+        
+        public void SetPlaca(string placa)
+        {
+           _placa = placa;
+        }
+        
+        public string GetPlaca()
+        {
+            return _placa;
+        }
+        
+        public void Setmodelo(string modelo)
+        {
+            _modelo = modelo;
+        }
+        
+        public  string GetModelo()
+        {
+            return _modelo;
+        }
+       
+        public void SetMarca(string marca)
+        {
+            _marca = marca;
+        }
+        public string GetMarca()
+        {
+            return _marca;
+        }
+
+
+        public void SetAno(double ano)
+        {
+            _ano = ano;
+        }
+        public double GetAno()
+        {
+            return _ano;
+        }
+
+
+
+        public void SetKilometragem(double kilometragem)
+        {
+            _kilometragem = kilometragem;
+        }
+        public double GetKilometragem()
+        {
+            return _kilometragem;
+        }
+
+
+        public void SetCaptanque(double captanque)
+        {
+            _captanque = captanque;
+        }
+        public double GetCaptanque()
+        {
+            return _captanque;
+        }
+
+
+
+        public void SetPotencia(double potencia)
+        {
+            _potencia = potencia;
+        }
+        public double GetPotencia()
+        {
+            return _potencia;
+        }
+    }
+}
---
+++ Exercícios/Exercicio10/Exercicio10/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Exercicio10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtmarca = new System.Windows.Forms.TextBox();
+            this.txtano = new System.Windows.Forms.TextBox();
+            this.txtmodelo = new System.Windows.Forms.TextBox();
+            this.txtkilometragem = new System.Windows.Forms.TextBox();
+            this.txtplaca = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtpotencia = new System.Windows.Forms.TextBox();
+            this.txtcaptanque = new System.Windows.Forms.TextBox();
+            this.Cadst = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtBsc = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Modelo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Marca";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ano";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Kilometragem";
+            // 
+            // txtmarca
+            // 
+            this.txtmarca.Location = new System.Drawing.Point(114, 130);
+            this.txtmarca.Name = "txtmarca";
+            this.txtmarca.Size = new System.Drawing.Size(100, 20);
+            this.txtmarca.TabIndex = 5;
+            // 
+            // txtano
+            // 
+            this.txtano.Location = new System.Drawing.Point(114, 160);
+            this.txtano.Name = "txtano";
+            this.txtano.Size = new System.Drawing.Size(100, 20);
+            this.txtano.TabIndex = 6;
+            // 
+            // txtmodelo
+            // 
+            this.txtmodelo.Location = new System.Drawing.Point(114, 69);
+            this.txtmodelo.Name = "txtmodelo";
+            this.txtmodelo.Size = new System.Drawing.Size(100, 20);
+            this.txtmodelo.TabIndex = 7;
+            this.txtmodelo.Text = "54";
+            // 
+            // txtkilometragem
+            // 
+            this.txtkilometragem.Location = new System.Drawing.Point(114, 193);
+            this.txtkilometragem.Name = "txtkilometragem";
+            this.txtkilometragem.Size = new System.Drawing.Size(100, 20);
+            this.txtkilometragem.TabIndex = 8;
+            // 
+            // txtplaca
+            // 
+            this.txtplaca.Location = new System.Drawing.Point(114, 102);
+            this.txtplaca.Name = "txtplaca";
+            this.txtplaca.Size = new System.Drawing.Size(100, 20);
+            this.txtplaca.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Placa";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(26, 226);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Cap. do Tanque";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(26, 252);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Potência";
+            // 
+            // txtpotencia
+            // 
+            this.txtpotencia.Location = new System.Drawing.Point(114, 252);
+            this.txtpotencia.Name = "txtpotencia";
+            this.txtpotencia.Size = new System.Drawing.Size(100, 20);
+            this.txtpotencia.TabIndex = 13;
+            // 
+            // txtcaptanque
+            // 
+            this.txtcaptanque.Location = new System.Drawing.Point(114, 223);
+            this.txtcaptanque.Name = "txtcaptanque";
+            this.txtcaptanque.Size = new System.Drawing.Size(100, 20);
+            this.txtcaptanque.TabIndex = 14;
+            this.txtcaptanque.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // Cadst
+            // 
+            this.Cadst.Location = new System.Drawing.Point(71, 289);
+            this.Cadst.Name = "Cadst";
+            this.Cadst.Size = new System.Drawing.Size(75, 23);
+            this.Cadst.TabIndex = 15;
+            this.Cadst.Text = "Cadastrar Carro";
+            this.Cadst.UseVisualStyleBackColor = true;
+            this.Cadst.Click += new System.EventHandler(this.Cadst_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(51, 26);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(132, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "CADASTRO DE CARROS";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(304, 26);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(109, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "BUSCA DE CARROS";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(286, 72);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(34, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Placa";
+            // 
+            // txtBsc
+            // 
+            this.txtBsc.Location = new System.Drawing.Point(326, 69);
+            this.txtBsc.Name = "txtBsc";
+            this.txtBsc.Size = new System.Drawing.Size(100, 20);
+            this.txtBsc.TabIndex = 19;
+            this.txtBsc.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(326, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 494);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtBsc);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.Cadst);
+            this.Controls.Add(this.txtcaptanque);
+            this.Controls.Add(this.txtpotencia);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtplaca);
+            this.Controls.Add(this.txtkilometragem);
+            this.Controls.Add(this.txtmodelo);
+            this.Controls.Add(this.txtano);
+            this.Controls.Add(this.txtmarca);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Placa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtmarca;
+        private System.Windows.Forms.TextBox txtano;
+        private System.Windows.Forms.TextBox txtmodelo;
+        private System.Windows.Forms.TextBox txtkilometragem;
+        private System.Windows.Forms.TextBox txtplaca;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtpotencia;
+        private System.Windows.Forms.TextBox txtcaptanque;
+        private System.Windows.Forms.Button Cadst;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtBsc;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
