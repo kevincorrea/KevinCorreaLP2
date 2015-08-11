f0fa050368a1c565320e62b1fd6db86defc10ab5
Kevin Correa <kevin131kevin@gmail.com>
2015-08-11 17:45:11 -0300

    Exercicio Aula 11/08

---
+++ Aulas/Aula_1108/Aula_1108/MinhaString.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1108
+{
+    class MinhaString
+    {
+        string s;
+        int i;
+        string srt;
+        public MinhaString() {}
+      
+        public MinhaString(string str)
+        {
+            s = srt;
+        }
+
+
+        public string Substring(int inicio)
+        {
+            string pedaco = "";
+            for (var i = inicio; i < s.Length; i++)
+            {
+                pedaco += s[i];                          
+            }
+
+            return pedaco;
+        }
+
+        public string Substring(int inicio, int final)
+        {
+            string pedaco = "";
+            for(int i = inicio; i <= final; i++)
+            {
+                pedaco += s[i];
+            }
+
+            return pedaco;
+        }
+
+        public string[] Split(string s)
+        {
+            string [] partes = new string[10];
+
+            string pedaco = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+               
+                //pedaco;
+            }
+
+            return partes;
+        }
+
+    }
+}
---
+++ Aulas/Aula_1108/Aula_1108/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1108
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DateTime dtNasc = new DateTime();
+            //MinhaString m = new MinhaString("Olá Mundo");
+
+            //Console.WriteLine(m.Substring(1));
+            //Console.WriteLine(m.Substring(3, 5));
+
+            var bananas = "bruno,diogo,kevin".Split(',');
+
+            foreach (var banana in bananas)
+            {
+                Console.WriteLine(banana);
+            }
+        }
+    }
+}
