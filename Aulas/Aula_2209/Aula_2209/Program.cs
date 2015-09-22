e57692548045ca011dff6ef7dadf81d5618aa8e0
Kevin Correa <kevin131kevin@gmail.com>
2015-09-22 17:51:23 -0300

    Aula_2209

---
+++ Aulas/Aula_2209/Aula_2209/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Aula_2209
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strConn = "Server=localhost;Database=test;Uid=root;Pwd=";
+            string nome = Console.ReadLine();
+            string query = "INSERT INTO PESSOA (Nome) VALUES ('" + nome + "')";
+        MySqlConnection conn = new MySqlConnection(strConn);
+        MySqlCommand command = new MySqlCommand();
+        command.Connection = conn;
+        command.CommandText = query;
+        conn.Open();
+        command.ExecuteNonQuery();
+        conn.Close();
+        }
+    }
+}
